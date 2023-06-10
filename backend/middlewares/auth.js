2aa5beaf9fbc99d1452782d539042b5c7d1d6ab3
Asya <pleyko.a@mail.ru>
2023-06-10 23:48:22 +0200

    а если так?

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,26 +1,31 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 
-const { NODE_ENV, JWT_SECRET = 'dev-secret' } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
-const auth = (req, _, next) => {
-  const token = req.cookies.jwt;
+const auth = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return next(new AuthorizationError('Ошибка: неверный электронный адрес или пароль'));
+  }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
 
   try {
     payload = jwt.verify(
       token,
-      `${NODE_ENV === 'production'
-        ? JWT_SECRET
-        : 'dev-secret'}`,
+      NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'
     );
   } catch (err) {
-    throw new AuthorizationError('Ошибка: неверный электронный адрес или пароль');
+    next(new AuthorizationError('Ошибка: неверный электронный адрес или пароль'));
   }
 
   req.user = payload;
 
-  next();
+  return next();
 };
 
 module.exports = auth;
