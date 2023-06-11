fc0fcf5e8de9e09caf5ceafce65eba110ac89c1e
Asya <pleyko.a@mail.ru>
2023-06-11 18:50:32 +0200

    cors1

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,12 +1,22 @@
 const allowedCors = [
   'https://pleykoa.nomoredomains.rocks',
+  'http://pleykoa.nomoredomains.rocks',
+  'localhost:3000',
 ];
 
+// eslint-disable-next-line consistent-return
 const cors = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
   }
+
+  const requestHeaders = req.headers['access-control-request-headers'];
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
   next();
 };
 
