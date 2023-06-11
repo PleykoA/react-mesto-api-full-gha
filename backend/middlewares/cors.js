d77293dfb61c01f90c36b4b9e65857bb23b48937
Asya <pleyko.a@mail.ru>
2023-06-11 18:25:45 +0200

    1254

---
+++ backend/middlewares/cors.js
@@ -0,0 +1,13 @@
+const allowedCors = [
+  'https://pleykoa.nomoredomains.rocks/',
+];
+
+const cors = (req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+};
+
+module.exports = cors;
